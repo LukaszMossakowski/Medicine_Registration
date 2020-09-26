ceb93e426afb677c8edf05cb6f461f1f561ff9af
Lukasz Mossakowski <lukasz.mossakowski@wp.pl>
2020-09-26 22:16:29 +0200

    password hiden behind *

--- Medicine_Registration2/settings.py
+++ Medicine_Registration2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6mg)ksyti8_kt35eqva+h(7ux8p4^tw627y!q&i8^um35t5%(y'
+SECRET_KEY = '************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': "medregister3",
         'HOST': 'localhost',
         'USER': 'postgres',
-        'PASSWORD': '*********'
+        'PASSWORD': '**********'
     }
 }
 
